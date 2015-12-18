6654655edfbe711477d9977a703a89ebb0c95545
William B. Wettersten <wwettersten@zendesk.com>
2015-12-17 19:34:14 -0800

    randomize to screen size

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
+  var colors = ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
 
-  var colors= ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
-  
   for (var i = 0; i <= 25; i++) {
-  	var topcord= Math.random() * 500;
-  	var leftcord= Math.random() * 500;
-  	var color= colors[Math.floor(Math.random() * 8)];
+    var topcord = Math.random() * $(window).height();
+    var leftcord = Math.random() * $(window).width();
+    var color = colors[Math.floor(Math.random() * 8)];
 
-  	$('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
+    $('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
   };
 });
 
