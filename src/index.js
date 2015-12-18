279dfdf0e0c69e86f4c124362aa3e812b8f4e47e
David Brodskiy <David Brodskiy>
2015-12-17 19:22:42 -0800

    created a function that created lots of food

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert();
+$(document).ready(function() {
+
+  var colors= ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
+  
+  for (var i = 0; i <= 25; i++) {
+  	var topcord= Math.random() * 500;
+  	var leftcord= Math.random() * 500;
+  	var color= colors[Math.floor(Math.random() * 8)];
+
+  	$('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
+  };
+});
+
