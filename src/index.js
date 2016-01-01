4e31307bda4f847293945625b5a03c5d60a9e9d7
David Brodskiy <David Brodskiy>
2015-12-31 19:25:04 -0800

    changed speed of food

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
   var colors = ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
+  var maximum = 400;
 
-  for (var i = 0; i <= 100; i++) {
-    var topcord = Math.random() * $(window).height();
-    var leftcord = Math.random() * $(window).width();
-    var color = colors[Math.floor(Math.random() * 8)];
+	function placefood() {
+	  var topcord = Math.random() * $(window).height();
+	  var leftcord = Math.random() * $(window).width();
+	  var color = colors[Math.floor(Math.random() * 8)];
 
-    $('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
-  };
+	  $('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
+	}
+
+  //for (var i = 0; i <= 100; i++) {
+    //placefood();
+  //}
+
+  window.setInterval(function() {
+  	var currentnumber = $('.food').length;
+
+    if (currentnumber<maximum) {
+	  placefood();
+    }
+  }, 400);
+
 });
 
