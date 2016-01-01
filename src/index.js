9fd3c8f9c001d15e17cbaf8729454662da5b6dd2
David Brodskiy <David Brodskiy>
2015-12-31 18:58:30 -0800

    changed food

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var colors = ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
 
-  for (var i = 0; i <= 25; i++) {
+  for (var i = 0; i <= 100; i++) {
     var topcord = Math.random() * $(window).height();
     var leftcord = Math.random() * $(window).width();
     var color = colors[Math.floor(Math.random() * 8)];
