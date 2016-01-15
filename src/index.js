f1cb20f3aeafed1f2ee40285fffb4bc9612cc149
William B. Wettersten <wwettersten@classpass.com>
2016-01-14 18:48:57 -0800

    remove tabs

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
   var colors = ['red', 'purple', 'blue', 'black', 'teal', 'magenta', 'pink', 'orange'];
   var maximum = 400;
 
-	function placefood() {
-	  var topcord = Math.random() * $(window).height();
-	  var leftcord = Math.random() * $(window).width();
-	  var color = colors[Math.floor(Math.random() * 8)];
+  function placefood() {
+    var topcord = Math.random() * $(window).height();
+    var leftcord = Math.random() * $(window).width();
+    var color = colors[Math.floor(Math.random() * 8)];
 
-	  $('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
-	}
+    $('body').append($('<div class="food" style="top: ' + topcord + 'px; left: ' + leftcord + 'px; background-color: ' + color + ';"></div>'));
+  }
 
   //for (var i = 0; i <= 100; i++) {
     //placefood();
   //}
 
   window.setInterval(function() {
-  	var currentnumber = $('.food').length;
+    var currentnumber = $('.food').length;
 
     if (currentnumber<maximum) {
-	  placefood();
+    placefood();
     }
   }, 400);
 
